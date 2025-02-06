269d7b0f8e0964acde7e71805e4dfa443b5d1bd5
Wilberth Martínez <wilberthmtzpuc@gmail.com>
2025-02-05 22:47:39 -0500

    Se hizo la actividad de la primera lección

---
+++ tipos-de-dato.js
@@ -0,0 +1,21 @@
+
+// TIPOS DE DATOS:
+
+console.log("42 es un dato de tipo", typeof 42) // NUMBER
+
+console.log("Veinticinco es un dato de tipo", typeof "Veinticinco") // STRING
+
+console.log("-666 es un dato de tipo", typeof -666) // NUMBER
+
+console.log("true es un dato de tipo", typeof true) // BOOLEAN
+
+console.log("0 es un dato de tipo", typeof 0) // NUMBER
+
+console.log("null es un dato de tipo", typeof null) // OBJECT, se considera un objeto vacío
+
+console.log("undefined es un dato de tipo", typeof undefined) // UNDEFINED
+
+console.log("FALSE es un dato de tipo", typeof FALSE) // UNDEFINED, para que me aparezca BOOLEAN tengo que ponerlo en minúsculas
+
+console.log("false es un dato de tipo", typeof false) // BOOLEAN, Como en este ejemplo
+
