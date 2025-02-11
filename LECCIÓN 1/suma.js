158456e81d273b3a8930db56ec30ed91822f8044
Wilberth Martínez <wilberthmtzpuc@gmail.com>
2025-02-10 23:17:51 -0500

    Se modificó letras mayúsculas en la suma de tres números y se agrego leyenda de ingresar a la consola en actividad de tipos de datos

--- LECCIÓN 1/suma.js
+++ LECCIÓN 1/suma.js
@@ -9,9 +9,9 @@ var gusto2 = "ajedrez"
 console.log("Mi nombre es:", nombre, "tengo la edad de", edad, "años, ", "me gusta la", gusto1, "y tambien el", gusto2)
 
 
-var num1 = Number(prompt("ingresa el número 1"))
-var num2 = Number(prompt("ingresa el número 2"))
-var num3 = Number(prompt("ingresa el número 3"))
+var num1 = Number(prompt("Ingresa el número 1"))
+var num2 = Number(prompt("Ingresa el número 2"))
+var num3 = Number(prompt("Ingresa el número 3"))
 
 alert(`La suma de ${num1} + ${num2} + ${num3} es igual a ${num1 + num2 + num3}`)
 
