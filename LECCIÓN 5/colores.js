ef9cf03bad26a8a823f2d8a7543a2845a9a93007
Wilberth Martínez <wilberthmtzpuc@gmail.com>
2025-02-19 19:35:41 -0500

    Se linkeo el CSS con el HTML y se agregaron 8 colores más

--- LECCIÓN 5/colores.js
+++ LECCIÓN 5/colores.js
@@ -2,10 +2,10 @@ const buttonJS = document.getElementById("trigger");
 const bodyJS = document.getElementById("body");
 const titleJS = document.getElementById("title");
 
-colors = ['#F72585', '#B5179E', '#560BAD', '#480CA8', '#3F37C9', '#4361EE', '#4895EF', "#4CC9F0"];
+colors = ['#F72585', '#B5179E', '#560BAD', '#480CA8', '#3F37C9', '#4361EE', '#4895EF', "#4CC9F0", "#FF5733", "#FFBD33", "#33FF57", "#33FFF5", "#5733FF", "#FF33A8", "#FF3380", "#A833FF"];
 
 const randomNumber = () => {
-    return Math.floor(Math.random() * 8);
+    return Math.floor(Math.random() * 16);
 }
 
 buttonJS.addEventListener("click", (e)=>{
