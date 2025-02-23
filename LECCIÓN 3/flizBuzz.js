c8a39e5d4b3409fee5806353c0c40b9df49b4a2e
Wilberth Martínez <wilberthmtzpuc@gmail.com>
2025-02-23 10:39:59 -0500

    Se hizo el código de FlizzBuzz

--- LECCIÓN 3/flizBuzz.js
+++ LECCIÓN 3/flizBuzz.js
@@ -1,13 +1,14 @@
 for(let index=1; index<=100; index++){
-    if(index % 3 = 0){
-        console.log("Fizz")
+
+    if(index % 3 === 0 && index % 5 === 0){
+        console.log(index,"FlizzBuzz")
+    }
+    else if(index % 3 === 0){
+        console.log(index, "Flizz")
     }   
-    else if(index % 5 = 0){
-        console.log("Buzz")
-    }
-    else if(index % 3 = 0 && index % 5 = 0){
-        console.log("FizzBuzz")
-    }
+    else if(index % 5 === 0){
+        console.log(index, "Buzz")
+    }   
     else{
         console.log(index)
     }
