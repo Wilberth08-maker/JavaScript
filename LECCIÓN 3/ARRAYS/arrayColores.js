ac290273e71c177009496e27bd48b0c3daa00b88
Wilberth Martínez <wilberthmtzpuc@gmail.com>
2025-02-24 22:17:38 -0500

    Se creó el código JS para el array del ejercicio práctico

---
+++ LECCIÓN 3/ARRAYS/arrayColores.js
@@ -0,0 +1,11 @@
+let colores = ["rojo", "verde", "amarillo", "azul"]; // Crear el array
+
+console.log(colores[1]); // Imprimir el segundo color del array
+
+colores[2] = "rosa"; // Modoficar el tercer color
+
+colores.push("morado"); // Agregar un color al final del array  
+
+colores.shift() // Quitar el primer elemento del array
+
+console.log(colores); // Imprimir el array modificado en la consola
