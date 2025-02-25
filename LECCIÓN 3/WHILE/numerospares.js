7396c7691d284a425cd03e676765cf6f455556bb
Wilberth Martínez <wilberthmtzpuc@gmail.com>
2025-02-24 21:30:16 -0500

    Se creo el código para contar los números del 10 al 1 y mostrar en la consola los números pares, todo esto con el ciclo While

--- LECCIÓN 3/WHILE/numerospares.js
+++ LECCIÓN 3/WHILE/numerospares.js
@@ -1,6 +1,10 @@
-index = 10;
-while(index >= 0){
-    console.log(index)
-    index--
-
+index = 10; // Inicializar en el 10
+while(index  >= 1){ // El While se detiene cuando el número sea menor que 1
+    
+    if(index % 2 === 0){ // Se divide entre dos el número y si el residuo es 0 significa que es par
+        
+        console.log(index)    
+    }
+    
+    index--; // Se decrementa uno
 }
