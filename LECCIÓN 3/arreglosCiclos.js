1d0b2f78d4bce61e5b380631d3075b8316dd2af8
Wilberth Martínez <wilberthmtzpuc@gmail.com>
2025-02-24 22:34:23 -0500

    Se añadio una parte del código JS

---
+++ LECCIÓN 3/arreglosCiclos.js
@@ -0,0 +1,9 @@
+let frutas = ["sandía",  "uvas", "melón", "platano", "manzana", "pitahaya", "pera", "uvas",
+    "papaya", "toronja", "mango", "pitahaya", "piña", "cereza", "durazno", "toronja",
+    "naranja", "sandía", "pitahaya", "manzana",  "uvas" ]
+
+let tiposDeFrutas = {};
+
+for(let ){
+
+}
