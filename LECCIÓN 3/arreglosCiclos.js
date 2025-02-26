0e4bad789021d104af214e839ccf3f59eb6298c6
Wilberth Martínez <wilberthmtzpuc@gmail.com>
2025-02-26 18:22:42 -0500

    Se terminó el código del proyecto de la lección 3

--- LECCIÓN 3/arreglosCiclos.js
+++ LECCIÓN 3/arreglosCiclos.js
@@ -1,9 +1,23 @@
+// Proyecto. Lección 3 - Arreglos y cliclos en JavaScript
+
 let frutas = ["sandía",  "uvas", "melón", "platano", "manzana", "pitahaya", "pera", "uvas",
     "papaya", "toronja", "mango", "pitahaya", "piña", "cereza", "durazno", "toronja",
-    "naranja", "sandía", "pitahaya", "manzana",  "uvas" ]
+    "naranja", "sandía", "pitahaya", "manzana",  "uvas", "cereza"]
 
-let tiposDeFrutas = {};
+let cantidadDeFrutas = {}; // Se define el objeto para almacenar la cantidad de cada tipo de fruta
 
-for(let ){
-
+for(let i=0; i < frutas.length; i++ ){     // Se crea un for que vaya desde 0 hasta frutas.lenght y se incremente en 1
+   
+    let fruta = frutas[i];      // Se define la variabñe fruta, el cual es frutas en la posición de i.
+   
+    if(cantidadDeFrutas[fruta] !== undefined){ // Si ya tiene un valor y no es undefined:   
+       
+        cantidadDeFrutas[fruta]++;            // Se incrementa 1
+    }
+    
+    else{
+        cantidadDeFrutas[fruta]=1;         // De lo contrario solo hay uno
+    }
 }
+
+console.log(cantidadDeFrutas)
