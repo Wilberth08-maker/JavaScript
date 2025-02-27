948226d616008dfd2548f5ad5035c1d134f38a1d
Wilberth Martínez <wilberthmtzpuc@gmail.com>
2025-02-26 19:43:57 -0500

    Se creo el código para el conversor de grados de temperatura den JS

---
+++ LECCIÓN 4/temperatura.js
@@ -0,0 +1,13 @@
+function fahrenheitACelcius(fahrenheit){        // Fahrenheit a Celcius
+    return (fahrenheit - 32) * 5/9;
+}
+
+function celciusAFahrenheit(celcius){       // Celcius a Fahrenheit
+    return (celcius * 9/5) + 32;
+}
+
+// Llamar a las funciones
+
+console.log("20°C en Fahrenheit es igual a: ", celciusAFahrenheit(20));
+console.log("68°F en Celcius es igual a: ", fahrenheitACelcius(68));
+console.log("32°F en Celsius es igual a: ", fahrenheitACelcius(32));
