e551e9449295987b17506d0ffdda1f6210179767
Wilberth Martínez <wilberthmtzpuc@gmail.com>
2025-02-28 15:56:57 -0500

    Se modificó el nombre de la actividad FizzBuzz, ya que tenía Flizz en lugar de Fizz

--- LECCIÓN 3/fizzBuzz.js
+++ LECCIÓN 3/fizzBuzz.js
@@ -5,10 +5,10 @@ for(let index=1; index<=100; index++){
 
     // Verificar si el número es múltiplo de 3 y de 5
     if(index % 3 === 0 && index % 5 === 0){
-        console.log(index,"FlizzBuzz"); // Imprimir FlizzBuzz
+        console.log(index,"FizzBuzz"); // Imprimir FizzBuzz
     }
     else if(index % 3 === 0){ // Si solo es múltiplo de 3
-        console.log(index, "Flizz"); // Imprimir Flizz
+        console.log(index, "Fizz"); // Imprimir Fizz
     }   
     else if(index % 5 === 0){ // Si solo es múltiplo de 5
         console.log(index, "Buzz"); // Imprimir Buzz
