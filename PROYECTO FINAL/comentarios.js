7cff75c49433a7a2e91731fe03007dca46f00b77
Wilberth Martínez <wilberthmtzpuc@gmail.com>
2025-02-27 22:34:01 -0500

    Se añadio un form para agregar el nombre del autor del comentario, comentarios.js

--- PROYECTO FINAL/comentarios.js
+++ PROYECTO FINAL/comentarios.js
@@ -1,8 +1,13 @@
 document.getElementById("comentarioForm").addEventListener("submit", (evento) => {
     evento.preventDefault();
 
+    let autorTexto = document.getElementById("autorInput").value;
     let comentarioTexto = document.getElementById("comentarioInput").value;
     let fechaHora = new Date().toLocaleString();
+
+    if(autorTexto.trim() === ""){
+        autorTexto = "ANONIMO"
+    };
  
     if(comentarioTexto.trim() === "") return;
 
@@ -10,14 +15,15 @@ document.getElementById("comentarioForm").addEventListener("submit", (evento) =>
     comentarioDiv.classList.add("comentario")
 
     comentarioDiv.innerHTML =`
+    <p class="autor">${autorTexto}</p>
     <p>${comentarioTexto}</p>
         <span class="fecha">${fechaHora}</span>
     <button class = "eliminar" > Eliminar comentario </button>
-    `;       
-    
+    `;    
 
     document.getElementById("comentarios").appendChild(comentarioDiv);
-    document.getElementById("comentarioInput").value = "";
+    document.getElementById("autorInput").value = "";
+    document.getElementById("comentarioInput").value = "";    
 
     comentarioDiv.querySelector(".eliminar").addEventListener("click",() => {
         comentarioDiv.remove();
